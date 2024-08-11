c8f7a2da4af6dc06049045b8c3c073babf0d216b
edsardgrisel <edsardgrisel1@gmail.com>
2024-08-11 21:38:19 +0200

    front end now complete. editing, adding photos now works.

---
+++ src/components/EditPhotoCard.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { Button } from "react-bootstrap";
+
+
+export default function EditPhotoCard({ folderId, photoId, onDelete }) {
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [photo, setPhoto] = useState({});
+    const [showModal, setShowModal] = useState(false); // Declare the state variables
+
+
+
+    const cardStyle = {
+        border: "1px solid black",
+        borderRadius: "10px",
+        padding: "10px",
+        margin: "10px",
+        boxSizing: "border-box",
+        textAlign: "center",
+        width: "200px",
+        flex: "1 1 30%",
+        transition: "transform 0.3s ease, box-shadow 0.3s ease", // Smooth transition for hover effects
+        cursor: "pointer", // Changes cursor to pointer on hover
+        backgroundColor: "#ffffff",
+    };
+
+    // Inline styles for the image container
+    const imageContainerStyle = {
+        width: "100%",
+        paddingTop: "75%",
+        position: "relative",
+    };
+
+    // Inline styles for the image
+    const imageStyle = {
+        position: "absolute",
+        top: "0",
+        left: "0",
+        width: "100%",
+        height: "100%",
+        objectFit: "contain",
+        borderRadius: "10px",
+    };
+
+    // Hover styles (can be applied via inline styles, but CSS is more suitable for hover effects)
+    const hoverStyle = {
+        transform: "scale(1.05)", // Slightly scale up the card
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)", // Add shadow to highlight
+    };
+
+    const fetchAndSetPhoto = async () => {
+        try {
+            const response = await axios.get(`http://localhost:5000/folders/${folderId}/photos/${photoId}`);
+            setPhoto(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.error(`Error fetching photos in folder card. FolderId: ${folderId}. PhotoId ${photoId}`, error);
+        }
+        setIsLoading(false);
+    }
+
+    const confirmDelete = async () => {
+        // Add your delete confirmation logic here
+        try {
+            const response = await axios.delete(`http://localhost:5000/folders/${folderId}/photos/${photoId}`);
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error fetching photos in folder card:', error);
+        }
+
+        console.log('Delete confirmed');
+        setShowModal(false);
+        onDelete(photoId);
+    };
+
+    const handleDeleteClick = () => {
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
+    useEffect(() => {
+        fetchAndSetPhoto();
+    }, []);
+
+    return (
+        <div
+            className="folder-card"
+            style={cardStyle}
+            onMouseOver={(e) => e.currentTarget.style.transform = hoverStyle.transform}
+            onMouseOut={(e) => e.currentTarget.style.transform = "none"}
+        >
+            {isLoading || !photo ? <p>Loading...</p> :
+                <div>
+                    <Button onClick={() => handleDeleteClick()} variant="danger"><RiDeleteBin5Line /></Button>
+                    <div style={imageContainerStyle}>
+                        <img
+                            src={photo.url}
+                            style={imageStyle}
+                        />
+                    </div>
+                </div>
+            }
+            {showModal && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <p>{`Are you sure you want to delete this photo?`}</p>
+                        <button onClick={confirmDelete}>Delete</button>
+                        <button onClick={handleCloseModal}>Cancel</button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
--- src/pages/EditFolder.js
+++ src/pages/EditFolder.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from "react";
 import axios from 'axios';
+import EditPhotoCard from '../components/EditPhotoCard';
 
 export default function EditFolder() {
     const { id } = useParams();
     const [folder, setFolder] = useState({});
     const [loading, setLoading] = useState(true);
+    // This should eventually be an array of photos
+    const [newPhoto, setNewPhoto] = useState({ url: '', date: '' });
 
     const fetchFolder = async () => {
         try {
@@ -23,29 +26,100 @@ export default function EditFolder() {
         fetchFolder();
     }, []);
 
-    return (
-        <div>
-            <h2>{"Edit " + folder.title}</h2>
-            <form>
-                <div>
-                    <label htmlFor="title">Title:</label>
-                    <input type="text" id="title" name="title" value={folder.title} onChange={(e) => setFolder({ ...folder, title: e.target.value })} />
-                </div>
-                <div>
-                    <label htmlFor="location">Location:</label>
-                    <input type="text" id="location" name="location" value={folder.location} onChange={(e) => setFolder({ ...folder, location: e.target.value })} />
-                </div>
-                <div>
-                    <label htmlFor="startDate">Start Date:</label>
-                    <input type="text" id="startDate" name="startDate" value={folder.startDate} onChange={(e) => setFolder({ ...folder, startDate: e.target.value })} />
-                </div>
-                <div>
-                    <label htmlFor="endDate">End Date:</label>
-                    <input type="text" id="endDate" name="endDate" value={folder.endDate} onChange={(e) => setFolder({ ...folder, endDate: e.target.value })} />
-                </div>
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.put(`http://localhost:5000/folders/${id}`, folder);
+            console.log('Folder updated successfully');
+        } catch (error) {
+            console.error('Error updating folder:', error);
+        }
+    }
 
-                <button type="submit" >Save</button>
-            </form>
+    const handleAddPhoto = async (e) => {
+        e.preventDefault();
+        try {
+            if (!newPhoto.url || !newPhoto.date) {
+                console.error('Photo URL and date are required');
+                return;
+            }
+
+            console.log(`photo to be added: ${newPhoto.url + newPhoto.date}`);
+            const newFolder = {
+                ...folder,
+                photos: [...folder.photos, newPhoto]
+            };
+            const response = await axios.put(`http://localhost:5000/folders/${id}`, newFolder);
+            if (response.status === 200) {
+                setFolder(newFolder);
+                setNewPhoto({ url: '', date: '' });
+                fetchFolder();
+            }
+
+
+            console.log('Photo added successfully');
+        } catch (error) {
+            console.error(`Error adding photo: `, error);
+        }
+    }
+
+
+    const handleDeletePhoto = (photoId) => {
+        setFolder(prevFolder => ({
+            ...prevFolder,
+            photos: prevFolder.photos.filter(photo => photo._id !== photoId)
+        }));
+    }
+
+    return (
+        <div style={{ border: '1px solid black', padding: '20px', borderRadius: '5px' }}>
+            <h2>{"Edit " + folder.title}</h2>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '10px' }}>
+                        <label htmlFor="title">Title:</label>
+                        <input type="text" id="title" name="title" value={folder.title} onChange={(e) => setFolder({ ...folder, title: e.target.value })} style={{ width: '30%' }} />
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '10px' }}>
+                        <label htmlFor="location">Location:</label>
+                        <input type="text" id="location" name="location" value={folder.location} onChange={(e) => setFolder({ ...folder, location: e.target.value })} style={{ width: '30%' }} />
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '10px' }}>
+                        <label htmlFor="startDate">Start Date:</label>
+                        <input type="text" id="startDate" name="startDate" value={folder.startDate} onChange={(e) => setFolder({ ...folder, startDate: e.target.value })} style={{ width: '30%' }} />
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '10px' }}>
+                        <label htmlFor="endDate">End Date:</label>
+                        <input type="text" id="endDate" name="endDate" value={folder.endDate} onChange={(e) => setFolder({ ...folder, endDate: e.target.value })} style={{ width: '30%' }} />
+                    </div>
+
+                    <button type="submit">Save</button>
+                </form>
+
+                <form onSubmit={handleAddPhoto}>
+                    <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '10px' }}>
+                        <label htmlFor="url">Url:</label>
+                        <input type="text" id="url" value={newPhoto.url} name="url" onChange={(e) => setNewPhoto({ ...newPhoto, url: e.target.value })} style={{ width: '30%' }} />
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '10px' }}>
+                        <label htmlFor="date">Date (yyyy-mm-dd)</label>
+                        <input type="text" id="date" value={newPhoto.date} name="date" onChange={(e) => setNewPhoto({ ...newPhoto, date: e.target.value })} style={{ width: '30%' }} />
+                    </div>
+
+
+                    <button type="submit">Save</button>
+                </form>
+            </div>
+
+            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                {loading ? (<p>Loading...</p>) : (
+                    folder.photos.map(photo => (
+                        <div key={photo._id} style={{ width: '25%', padding: '10px' }}>
+                            <EditPhotoCard folderId={id} photoId={photo._id} onDelete={handleDeletePhoto} />
+                        </div>
+                    ))
+                )}
+            </div>
         </div>
     );
 
